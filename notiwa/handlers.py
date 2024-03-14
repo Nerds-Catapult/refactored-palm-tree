684ec9b7077438defa3d393208d6f7b89c3481a7
Domains18 <gibsonsgibson88@gmail.com>
2024-03-14 09:52:37 +0300

    Implement new feature XYZ

---
+++ notiwa/handlers.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+__all__ = [
+    "MessageHandler",
+    "CallbackButtonHandler",
+    "CallbackSelectionHandler",
+    "RawUpdateHandler",
+    "MessageStatusHandler",
+    "TemplateStatusHandler",
+    "FlowCompletionHandler",
+    "FlowRequestHandler",
+    "ChatOpenedHandler",
+]
